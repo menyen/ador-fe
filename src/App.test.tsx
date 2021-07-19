946e315eb55fcd907001b94344b8bef0601f1dcb
Ng Men Yen <men.yen@gmail.com>
2021-07-19 18:42:24 -0300

    Fix UT and update README to reflect yarn usage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Data do último questionário/i);
   expect(linkElement).toBeInTheDocument();
 });
