f2b7a94678da2c93f7b5aa1092c7ef9d76dabf3e
Ng Men Yen <men.yen@gmail.com>
2021-09-18 20:42:28 -0300

    Add patient profile header

--- src/components/patient/PatientPage.tsx
+++ src/components/patient/PatientPage.tsx
@@ -13,10 +13,17 @@ import { KeyboardArrowLeft, KeyboardArrowRight } from '@material-ui/icons';
 import React from 'react';
 import { green } from '@material-ui/core/colors';
 import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
+import logo from '../../image/logo.svg';
+import logoWhite from '../../image/logo-white.svg';
 import onBoardStep1 from '../../image/onboard-step-1.svg';
 import onBoardStep2 from '../../image/onboard-step-2.svg';
 import onBoardStep3 from '../../image/onboard-step-3.svg';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Grid from '@material-ui/core/Grid';
 
 function getHasOnboardedLocalStorage(): boolean {
   const hasGoneOnboard = localStorage.getItem('onboard') || 'false';
@@ -30,7 +37,7 @@ function setHasOnboardedLocalStorage(isOnboard: boolean): void {
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      marginTop: '64px',
+      // marginTop: '64px',
       width: '100%',
       // padding: theme.spacing(4),
       height: '100vh',
@@ -42,12 +49,21 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: '1rem',
       height: '150px',
     },
+    logo: {
+      marginBottom: '2rem',
+    },
+    logoWhite: {
+      margin: '1rem auto',
+    },
     img: {
       height: 255,
       maxWidth: 400,
       overflow: 'hidden',
       display: 'block',
       width: '100%',
+    },
+    profileMenu: {
+      backgroundColor: '#CEEBEA',
     },
   })
 );
@@ -83,9 +99,74 @@ const LoginPatientButton = withStyles((theme: Theme) => ({
   },
 }))(IconButton);
 
+interface OnBoardProps {
+  activeStep: number;
+  maxSteps: number;
+  handleNext: () => void;
+  handleBack: () => void;
+  setHasOnboarded: (onboarded: boolean) => void;
+}
+
+function OnBoard(props: OnBoardProps) {
+  const classes = useStyles();
+
+  const { activeStep, maxSteps, handleNext, handleBack, setHasOnboarded } =
+    props;
+  return (
+    <>
+      <img src={logo} className={classes.logo} />
+      <img
+        className={classes.img}
+        src={tourSteps[activeStep].imgPath}
+        alt={tourSteps[activeStep].label}
+      />
+      <Paper square elevation={0} className={classes.tourTitle}>
+        <Typography variant="h6" component="h1">
+          {tourSteps[activeStep].title}
+        </Typography>
+      </Paper>
+      <Paper square elevation={0} className={classes.tourLabel}>
+        <Typography variant="body1">{tourSteps[activeStep].label}</Typography>
+      </Paper>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        variant="dots"
+        activeStep={activeStep}
+        nextButton={
+          <LoginPatientButton
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps}
+          >
+            <KeyboardArrowRight />
+          </LoginPatientButton>
+        }
+        backButton={
+          <LoginPatientButton
+            size="small"
+            onClick={handleBack}
+            disabled={activeStep === 0}
+          >
+            <KeyboardArrowLeft />
+          </LoginPatientButton>
+        }
+      />
+      <Button
+        size="small"
+        onClick={() => {
+          setHasOnboardedLocalStorage(true);
+          setHasOnboarded(true);
+        }}
+      >
+        Pular etapa
+      </Button>
+    </>
+  );
+}
+
 export default function PatientPage() {
   const classes = useStyles();
-  const theme = useTheme();
   const [activeStep, setActiveStep] = React.useState(0);
   const [hasOnboarded, setHasOnboarded] = React.useState(
     getHasOnboardedLocalStorage()
@@ -108,58 +189,56 @@ export default function PatientPage() {
   return (
     <div className={classes.root}>
       {hasOnboarded ? (
-        <div>Já passou pelas boas vindas!</div>
-      ) : (
         <>
-          <img
-            className={classes.img}
-            src={tourSteps[activeStep].imgPath}
-            alt={tourSteps[activeStep].label}
-          />
-          <Paper square elevation={0} className={classes.tourTitle}>
-            <Typography variant="h6" component="h1">
-              {tourSteps[activeStep].title}
+          <Accordion className={classes.profileMenu}>
+            <AccordionSummary
+              expandIcon={<MenuIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Grid container>
+                <Grid item xs={12}>
+                  <img
+                    src={logoWhite}
+                    alt="Logo da Ador em branco"
+                    className={classes.logoWhite}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="h4" component="h1">
+                    Olá, André!
+                  </Typography>
+                </Grid>
+              </Grid>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography variant="body1">
+                Abaixo estão os questionários que foram Solicitados para que
+                você responda. Com os resultados destes questionários, o
+                profissional Irá conhecer e analisar sua dor e assim, planejar
+                seu tratamento.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          {/* <Paper className={classes.profileMenu}>
+            <Typography variant="h4" component="h1">
+              Olá, André!
             </Typography>
-          </Paper>
-          <Paper square elevation={0} className={classes.tourLabel}>
             <Typography variant="body1">
-              {tourSteps[activeStep].label}
+              Abaixo estão os questionários que foram Solicitados para que você
+              responda. Com os resultados destes questionários, o profissional
+              Irá conhecer e analisar sua dor e assim, planejar seu tratamento.{' '}
             </Typography>
-          </Paper>
-          <MobileStepper
-            steps={maxSteps}
-            position="static"
-            variant="dots"
-            activeStep={activeStep}
-            nextButton={
-              <LoginPatientButton
-                size="small"
-                onClick={handleNext}
-                disabled={activeStep === maxSteps}
-              >
-                <KeyboardArrowRight />
-              </LoginPatientButton>
-            }
-            backButton={
-              <LoginPatientButton
-                size="small"
-                onClick={handleBack}
-                disabled={activeStep === 0}
-              >
-                <KeyboardArrowLeft />
-              </LoginPatientButton>
-            }
-          />
-          <Button
-            size="small"
-            onClick={() => {
-              setHasOnboardedLocalStorage(true);
-              setHasOnboarded(true);
-            }}
-          >
-            Pular etapa
-          </Button>
+          </Paper> */}
         </>
+      ) : (
+        <OnBoard
+          activeStep={activeStep}
+          maxSteps={maxSteps}
+          handleNext={handleNext}
+          handleBack={handleBack}
+          setHasOnboarded={setHasOnboarded}
+        />
       )}
     </div>
   );
